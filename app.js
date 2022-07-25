5b475d479808d112db3b4eb05c523e99b9795ae9
tanveerprottoy <tanveer.prottoy@gmail.com>
2022-07-26 00:32:05 +0600

    add app.js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-const grpc = require("grpc");
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const packageDef = protoLoader.loadSync(
+    "user.proto",
+    {}
+);
+const grpcObj = grpc.loadPackageDefinition(
+    packageDef
+);
+const userPackage = grpcObj.userPackage;
+
+function createUser(
+    call,
+    callback
+) {
+    console.log(call);
+}
+
+function readUsers(
+    call,
+    callback
+) {
+    console.log(call);
+}
+
+const server = new grpc.Server();
+server.bind(
+    "0.0.0.0:50000",
+    grpc.ServerCredentials.createInsecure()
+);
+server.addService(
+    userPackage.UserService.service,
+    {
+        "createUser": createUser,
+        "readUsers": readUsers
+    }
+);
+server.start();
